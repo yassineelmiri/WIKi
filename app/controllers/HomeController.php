5e1c1ab90a1b1cc677d94c7014d73a51367f998d
YASSINE DEV <yassireelhima@gmail.com>
2024-01-06 21:47:21 +0100

    controller de Home

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'app/models/Wiki.php';
+require_once 'app/models/Category.php';
+require_once 'app/models/home.php';
+
+class HomeController {
+    public function index() {
+        $latestWikis = HomeModel::getLatestWikis();
+
+        $latestCategories = HomeModel::getLatestCategories();
+
+        // Afficher
+        include 'app/views/home.php';
+    }
+}
+?>
