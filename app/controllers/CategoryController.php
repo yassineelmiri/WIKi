332a5cc231618be453854e2ac8554ac95b89a93c
YASSINE DEV <yassireelhima@gmail.com>
2024-01-07 12:31:19 +0100

    modification de controller category

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -7,31 +7,35 @@ class CategoryController {
     // Action pour afficher toutes les catégories
     public function showAllCategories() {
         $categories = Category::getAllCategories();
-        // Vous pouvez passer $categories à votre vue (frontend) pour l'affichage.
+        include 'app/views/category.php';
     }
 
     // Action pour afficher une catégorie spécifique par son ID
     public function showCategoryById($categoryId) {
         $category = Category::getCategoryById($categoryId);
-        // Vous pouvez passer $category à votre vue (frontend) pour l'affichage.
+        include 'app/views/category.php';
     }
 
     // Action pour ajouter une nouvelle catégorie
     public function addCategory($name) {
         $newCategory = new Category(null, $name);
         $newCategory->save();
-        // Redirection ou autre logique après l'ajout.
+        header('Location: categories.php');
+        exit();
     }
 
     // Action pour mettre à jour une catégorie
     public function updateCategoryById($categoryId, $name) {
         Category::updateCategoryById($categoryId, $name);
-        // Redirection ou autre logique après la mise à jour.
+        header('Location: categories.php');
+        exit();
     }
 
     // Action pour supprimer une catégorie
     public function deleteCategoryById($categoryId){
         Category::deleteCategoryById($categoryId);
-        // Redirection ou autre logique après la suppression.
+        header('Location: categories.php');
+        exit();
     }
 }
+?>
