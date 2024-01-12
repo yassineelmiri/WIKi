c82aa1b3cb7632545bebf91fabc350393ffeada8
yassineelmiri <yassireelhima@gmail.com>
2024-01-12 12:14:26 +0100

    maintent je connecter database de wiki est travaille par xampp

---
+++ app/config/configuration.php
@@ -0,0 +1,8 @@
+<?php
+
+    /* Project Configuration */ 
+    define("WebsiteName", "wiki");
+    define("UrlRoot", "http://localhost/wiki");
+    define("AppRoot", dirname(dirname(__FILE__)));
+
+?>
